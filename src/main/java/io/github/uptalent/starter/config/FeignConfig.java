907d7d3f0c5011763e1058d47df1b1c1f184e20f
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-08-10 16:07:05 +0300

    Replace @Autowired with @RequiredArgsConstructor in FeignConfig

--- src/main/java/io/github/uptalent/starter/config/FeignConfig.java
+++ src/main/java/io/github/uptalent/starter/config/FeignConfig.java
@@ -2,7 +2,7 @@ package io.github.uptalent.starter.config;
 
 import feign.RequestInterceptor;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
@@ -16,14 +16,10 @@ import static io.github.uptalent.starter.util.Constants.USER_ID_KEY;
 import static io.github.uptalent.starter.util.Constants.USER_ROLE_KEY;
 
 @Configuration
+@RequiredArgsConstructor
 public class FeignConfig {
 
     private final HttpServletRequest request;
-
-    @Autowired
-    public FeignConfig(HttpServletRequest request) {
-        this.request = request;
-    }
 
     @Bean
     public RequestInterceptor requestInterceptor() {
