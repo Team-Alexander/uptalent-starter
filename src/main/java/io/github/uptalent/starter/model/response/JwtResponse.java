e7637d251a0c595638058a5509dc3bd892e8296b
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-08-26 19:06:16 +0300

    Add new models and update existing files
    Adding new files including ReportRequest, Author, ReportResponse, EmailMessageDetailInfo, and other models related to handling mail and event notifications. Constants.java has been updated with new string constants meant to facilitate improving communication features. pom.xml's version has been updated from 1.1.6 to 1.1.7. This major change is to support a new functionality in the system relating to event notifications, emails, and reports. The modification to AuthenticationFilter.java improves code readability.

---
+++ src/main/java/io/github/uptalent/starter/model/response/JwtResponse.java
@@ -0,0 +1,12 @@
+package io.github.uptalent.starter.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtResponse {
+    private String jwt;
+}
--- src/main/java/io/github/uptalent/starter/util/Constants.java
+++ src/main/java/io/github/uptalent/starter/util/Constants.java
@@ -6,4 +6,13 @@ public final class Constants {
     public static final String ACCESS_DENIED_MESSAGE = "You do not have the necessary permissions to perform this operation";
     public static final String USER_ID_KEY = "User-Id";
     public static final String USER_ROLE_KEY = "User-Role";
+    public static final String TALENT_SUFFIX = "-TALENT";
+    public static final String ADMIN_SUFFIX = "-ADMIN";
+    public static final String PROOFS_LINK = "proofs/";
+    public static final String SPONSOR_SUFFIX = "-SPONSOR";
+    public static final String SUBMISSIONS_LINK = "submissions/";
+    public static final String VACANCIES_LINK = "vacancies/";
+    public static final String TALENTS_LINK = "talents/";
+    public static final String SPONSORS_LINK = "sponsors/";
+    public static final double TOXICITY_THRESHOLD = 0.5;
 }
