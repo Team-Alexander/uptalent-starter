2256c5e32136203ae71eb9f96b6603c6ea197150
Mark <42696773+MarkHmnv@users.noreply.github.com>
2023-08-26 19:30:47 +0300

    Add Perspective API models (#12)

---
+++ src/main/java/io/github/uptalent/starter/model/common/TextItem.java
@@ -0,0 +1,12 @@
+package io.github.uptalent.starter.model.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TextItem {
+    private String text;
+}
